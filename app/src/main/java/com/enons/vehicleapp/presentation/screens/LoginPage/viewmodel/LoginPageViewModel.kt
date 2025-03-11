8aebc3ba3244a7ab20337b970763e8f67c91bbf6
Farouk Touil <farouktouil@hotmail.com>
2025-03-11 10:43:20 +0100

    upload auth

--- app/src/main/java/com/enons/vehicleapp/presentation/screens/LoginPage/viewmodel/LoginPageViewModel.kt
+++ app/src/main/java/com/enons/vehicleapp/presentation/screens/LoginPage/viewmodel/LoginPageViewModel.kt
@@ -1,1 +1,22 @@
 package com.enons.vehicleapp.presentation.screens.LoginPage.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+//import com.enons.vehicleapp.data.repository.FirebaseAuthRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class LoginPageViewModel @Inject constructor(
+    //private val authRepository: FirebaseAuthRepository
+) : ViewModel() {
+
+    //val authState: LiveData<AuthState> = authRepository.authState
+
+    fun login(email: String, password: String) {
+      //  authRepository.loginUser(email, password)
+    }
+}
+
+
+
--- app/src/main/java/com/enons/vehicleapp/presentation/screens/RegisterPage/viewmodel/RegisterViewModel.kt
+++ app/src/main/java/com/enons/vehicleapp/presentation/screens/RegisterPage/viewmodel/RegisterViewModel.kt
@@ -1,2 +1,19 @@
 package com.enons.vehicleapp.presentation.screens.RegisterPage.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+//import com.enons.vehicleapp.data.repository.FirebaseAuthRepository
+import com.enons.vehicleapp.presentation.screens.LoginPage.viewmodel.AuthState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class RegisterViewModel @Inject constructor(
+   // private val authRepository: FirebaseAuthRepository
+) : ViewModel() {
+    //val authState: LiveData<AuthState> = authRepository.authState
+
+    fun signup(email: String, password: String) {
+        //authRepository.registerUser(email, password)
+    }
+}
